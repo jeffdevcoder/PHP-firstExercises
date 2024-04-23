cfffc32c74df84d4cc9818dc70c95b99cecf4371
Jefferson Gonçalves <jeffdevcoder@gmail.com>
2024-04-22 21:21:34 -0300

    primitive types on php

---
+++ ex004/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php
+        /* $v = 20; */
+        // $v = 3e20; 
+        $v = true;
+
+        var_dump($v);
+        echo "\nO valor do boolean é: $v"
+    ?>
+</body>
+</html>
---
+++ ex005/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //$nome = "Jeff";
+        // $sobrenome = "Gonçalves";
+        // echo "$nome $sobrenome";
+
+        // const nome = "Jeff";
+        // const sobrenome = "Gonçalves";      
+        // echo "$nome" . sobrenome;
+
+        $nome = "Jeff";
+        $sobrenome = "Gonçalves";
+        echo "$nome \"sabo\" $sobrenome";
+    ?>
+</body>
+</html>
